282507e994a02f1c71a3967adb89c8fed0134705
Alfredo Mendoza <1930.kick@gmail.com>
2021-04-29 01:49:59 -0500

    Fix sw.js path

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ window.Components.menu = function (t = { open: !1 }) {
 
 if ('serviceWorker' in navigator) {
   addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('Service worker registered.', reg))
       .catch(err => console.warn('Error', err))
   })
